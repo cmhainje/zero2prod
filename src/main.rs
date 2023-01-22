482e53e6373be4155c736b7a542c0e065db6d78f
Connor Hainje <cmhainje@gmail.com>
2023-01-22 10:42:45 -0500

    use lazy postgres connection

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to postgres.");
+    let connection =
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection)?.await
